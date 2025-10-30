f2b5297eb4445a3eff5536b777def9efe904b73f
Tori Wu <cwu135@uottawa.ca>
2025-10-30 15:38:18 -0400

    Q2: CardsSet

--- myFile.cpp
+++ myFile.cpp
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 #include "myFile.h"
@@ -47,24 +49,75 @@ int main() {
 }
 
 void Card::write() {
-	if (val <= 10) {
-		cout << val << " of " << col << endl;
-	} else{
-		switch (val) {
-			case 11:
-				cout << "Jack of " << col << endl;
-				break;
-			case 12:
-				cout << "Queen of " << col << endl;
-				break;
-
-			case 13:
-				cout << "King of " << col << endl;
-				break;
-		
-			
-		}
+	string suites[4] = {"Clubs", "Diamonds", "Hearts", "Spades"};
+	switch (val) {
+		case 11:
+			cout << "Jack of " << suites[col] << endl;
+			break;
+		case 12:
+			cout << "Queen of " << suites[col] << endl;
+			break;
+		case 13:
+			cout << "King of " << suites[col] << endl;
+			break;
+		case 1:
+			cout << "Ace of " << suites[col] << endl;
+			break;
+		default:
+			cout << val << " of " << suites[col] << endl;
+			break;
 	}
-	
 }
 
+/**
+ * create new set of 52 cards
+ */
+void CardsSet::novSet() {
+	for (int i = 1; i <= 13; i++) {
+		for (int c = 0; c < 4; c++) {
+			set[number++] = Card(static_cast<color>(c), i);
+		}
+	}
+}
+
+/**
+ * shuffles cards
+ */
+void CardsSet::shuffle() {
+	srand(time(0));
+	for (int i = number - 1; i > 0; i--) { 
+        int j = rand() % (i + 1); 
+        swap(set[i], set[j]); 
+	}
+}
+
+/**
+ * take a card from the set
+ */
+Card CardsSet::take() {
+	if (number > 0) {
+		return set[--number]; // take from top (0 = bottom)
+	}
+	cout << "No more cards in set" << endl;
+	return Card();
+}
+
+/**
+ * put a card in the set
+ */
+void CardsSet::put(Card k) {
+	if (number < 52) {
+		set[number++] = k;
+	}
+}
+
+/**
+ * find the card at position 'no'
+ */
+Card CardsSet::lookIn(int no) {
+	if (no >= 1 && no <= number) {
+        return set[number - no]; // 1 = top card = total number of cards - 1
+	}
+	cout << "Out of range" << endl; 
+	return Card();
+}
